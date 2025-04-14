9202c76a0b02e1d9886cf549effd87d391d021bc
pw252 <67201191+pw252@users.noreply.github.com>
2025-04-14 20:58:53 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors())
 const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: "https://testcallclient-1ukd.vercel.app",
+    origin: "https://testcallclient-fsi7.vercel.app",
     methods: ["GET", "POST"],
   },
 })
